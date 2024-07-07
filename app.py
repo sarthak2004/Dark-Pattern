e85e9ef22d4c529fb64a98fcfbe6386a0d419a33
Saumil004 <agg.sa31082004@gmail.com>
2024-07-07 20:52:14 +0530

    first commit

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from flask import Flask, render_template, request, redirect, url_for,jsonify,flash
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 import nltk
 from nltk import WordNetLemmatizer
-import matplotlib.pyplot as plt
+# import matplotlvib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -15,13 +15,16 @@ import xgboost
 from xgboost import XGBClassifier
 from joblib import dump, load
 import joblib
-
-
 import csv
 import pandas as pd
 import numpy as np
 import re
 import requests
+import plotly.graph_objs as go
+from flask_pymongo import PyMongo
+
+
+
 
 
 
@@ -53,25 +56,17 @@ def return_sentences(tokens):
     return " ".join([word for word in tokens])
 
 
-rf= joblib.load("random_forest_model.joblib")
+rf= joblib.load("Random_forest_final.joblib")
 
 
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///user_dark_pattern.db'
-db=SQLAlchemy(app)
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+
+db = PyMongo(app).db
 
 
-class user(db.Model):
-    id= db.Column(db.Integer,primary_key=True)
-    first_name=db.Column(db.String)
-    last_name=db.Column(db.String)
-    email= db.Column(db.String)
-    mobile= db.Column(db.String)
-
-    def __repr__(self):
-        return '<Task %r' % self.id
 
 
 url=""
@@ -83,7 +78,10 @@ def receive_url():
 def process_link(link):
     options = Options()
     options.add_argument('--disable-dev-shm-usage')
+    # options.add_argument("--headless=new")
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+   
     driver.get(link)
     return driver
 
@@ -146,6 +144,13 @@ def array_to_csv(csv_data, filename):
 def index():
     return render_template('index.html')
 
+
+@app.route('/form-submit',methods=['GET','POST'])
+def form_submit():
+    if request.method=='POST':
+        flash(f"Dark Pattern reported successfully")
+    return render_template("index.html")
+    
 @app.route('/analysis', methods=['GET', 'POST'])
 def analysis():
     if request.method=='POST':
@@ -172,30 +177,24 @@ def analysis():
             df.drop_duplicates(inplace = True)
 
 
-            # print(df[column_name])
+            class_mapping= {0: 'forced action', 1: 'misdirection', 2: 'obstruction', 3: 'scarcity', 4: 'sneaking', 5: 'social proof', 6: 'urgency'}
 
-            # print(df)
-
-            tfidf_vectorizer = joblib.load("tfidf_vectorizer.joblib")
+            tfidf_vectorizer = joblib.load("vectorizer2.joblib")
 
             numpy_array = df[column_name].values
 
-            # print(numpy_array)
 
-            tfidf_array = tfidf_vectorizer.transform(df[column_name]).toarray()
+            tfidf_array = tfidf_vectorizer.transform(df[column_name])
 
-            # print(tfidf_array)
-
-            class_mapping = {0: 'Forced Action', 1: 'Misdirection', 2: 'Obstruction', 3: 'Scarcity', 4: 'Sneaking', 5: 'Social Proof', 6: 'Urgency'}
 
             probabilities = rf.predict_proba(tfidf_array)
 
             pre_predicted = rf.predict(tfidf_array)
 
-            rows_above_threshold = np.where((pre_predicted != 1) & (np.max(probabilities, axis=1) > 0.75))[0]
+            rows_above_threshold = np.where( (np.max(probabilities, axis=1) > 0.80))[0]
 
             if(rows_above_threshold.size<1):
-                return render_template('index.html')
+                return render_template('error.html',link=link)
 
             filtered_data = numpy_array[rows_above_threshold]
 
@@ -208,34 +207,40 @@ def analysis():
 
             frequency_df = pd.DataFrame({'Category': category_frequencies.index, 'Frequency': category_frequencies.values})
 
-            freq_data= frequency_df.values
+
+            numpy_data= (frequency_df).to_numpy()
+
+            result_val=result.values
+
+            numpy_data= (frequency_df).to_numpy()
             result_val=result.values
             print(result)
 
-            return render_template('analysis.html',freq_data=freq_data)
+            column1 = numpy_data[:, 0]
+            column2 = numpy_data[:, 1]
+
+            # Creating plotly graph
+            trace = go.Bar(x=column1, y=column2, name='Data')
+            layout = go.Layout(
+                title='Dark Pattern Found', 
+                xaxis=dict(title='Dark Pattern Category'), 
+                yaxis=dict(title='Occurences'),
+                autosize=False,
+                width=800,  # Adjust width as needed
+                height=600,  # Adjust height as needed
+                margin=dict(l=50, r=50, b=100, t=100)# Adjust margins as needed
+            )
+            fig = go.Figure(data=[trace], layout=layout)
+            graph = fig.to_html(full_html=False, default_height=500, default_width=700)
+
+            return render_template('analysis.html',graph=graph)
     
         else:
-            return "Invalid URL. Please provide a valid URL."
+            return render_template('not_scrappable.html',link=link)
     else:
         return render_template('analysis.html')
-        
-@app.route('/report',methods=['GET','POST'])
-def report():
-    if request.method=='POST':
-        fir_nam= request.form['firstname']
-        las_nam= request.form['lastname']
-        mob= request.form['phone']
-        mail= request.form['email']
-        new_user= user(first_name=fir_nam,last_name=las_nam,mobile=mob,email=mail)
 
-        try:
-            db.session.add(new_user)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return render_template('index.html')
-
-
+    
     
 
 if __name__ == '__main__':
