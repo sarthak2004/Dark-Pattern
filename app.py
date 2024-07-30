96ba4b999a575f9529f3c0486da48a1129e3daa1
sarthak2004 <sarthakishu2004@gmail.com>
2024-07-31 00:35:35 +0530

    adjusted port

--- app.py
+++ app.py
@@ -283,6 +283,6 @@ def analysis():
     else:
         return render_template('analysis.html')
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port)
