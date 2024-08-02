71e9da79d88509899b3b55a8f14e8b5ebc30551d
sarthak2004 <sarthakishu2004@gmail.com>
2024-08-03 01:12:35 +0530

    adding chrome

--- setup-chrome.sh
+++ setup-chrome.sh
@@ -1,30 +1,20 @@
-#!/usr/bin/env bash
+#!/bin/bash
 set -o errexit
-set -o nounset
-set -o pipefail
 
 STORAGE_DIR=/opt/render/project/.render
-CHROME_DIR=$STORAGE_DIR/chrome
 
-if [[ ! -d $CHROME_DIR ]]; then
+if [[ ! -d $STORAGE_DIR/chrome ]]; then
   echo "...Downloading Chrome"
-  mkdir -p $CHROME_DIR
-  cd $CHROME_DIR
-
-  if ! wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb; then
-    echo "Failed to download Google Chrome. Exiting."
-    exit 1
-  fi
-
-  if ! dpkg -x google-chrome-stable_current_amd64.deb $CHROME_DIR; then
-    echo "Failed to extract Google Chrome package. Exiting."
-    exit 1
-  fi
-
-  rm google-chrome-stable_current_amd64.deb
-
-  # Ensure we’re in the correct working directory
-  cd /app
+  mkdir -p $STORAGE_DIR/chrome
+  cd $STORAGE_DIR/chrome
+  wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+  dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+  rm ./google-chrome-stable_current_amd64.deb
+  cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
 fi
+
+# Install Python dependencies
+echo "...Installing Python dependencies"
+pip install -r requirements.txt
